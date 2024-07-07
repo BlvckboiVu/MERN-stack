9290df1ca814903043bd0b46869054a4963eecc4
Dejola <dejolalufadeju@gmail.com>
2024-07-07 10:16:48 +0100

    first commit

---
+++ reactNew/react-job/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <header className="bg-purple-400 flex items-center justify-between px-[10%] py-4">
+        <img src="" alt="Logo" />
+        <nav>
+          <a href="">Home</a>
+          <button className="bg-purple-700 px-3 py-1 text-white">Sign Up</button>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default App;
